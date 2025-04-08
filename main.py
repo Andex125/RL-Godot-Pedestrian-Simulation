56a9bd9b6439cd81c1f6b31747482dbf29f59698
Andex125 <andreaaivaliotis@gmail.com>
2025-04-08 22:29:30 +0200

    adjustments to the training model

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from scripts.utils.Runner import Runner
 
 # Instantiating runner and starting training
 runner = Runner(
-    config_path="scripts/configs/sensitivity_studies/net_256_128_64.yaml",
+    config_path="scripts/configs/base_config.yaml",
     curriculum_path="scripts/configs/curriculum/curriculum_config.yaml",
-    run_name="Andrea/prova2",
+    run_name="Andrea/ProvaModello8",
 )
 runner.run()
