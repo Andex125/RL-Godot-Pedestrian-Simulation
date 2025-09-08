1cab244afa0964416826223c98715b8952ac671d
Andex125 <andreaaivaliotis@gmail.com>
2025-09-08 19:50:25 +0200

    b1

--- godot/environments/scripts/randomizer/passage.gd
+++ godot/environments/scripts/randomizer/passage.gd
@@ -4,7 +4,19 @@ extends Randomizer
 
 # Override the entity with the specific node
 func _ready():
+	# Verifica che il nodo passage esista
+	if passage == null:
+		push_error("Nodo Passage non trovato in " + str(get_path()))
+		return
+	
 	entity = passage
 	offset = 0  # Specific offset for Passage
 	areas = find_children("CollisionShapePassage*")
-	set_random()
+	
+	# Verifica che ci siano aree
+	if areas.is_empty():
+		push_error("Nessuna CollisionShapePassage trovata in " + str(get_path()))
+		return
+	
+	# Usa call_deferred per assicurarti che tutto sia pronto
+	call_deferred("set_random")
